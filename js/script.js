b43e12b4158bec6adad46abbe807a07c320df284
Mauro <mauro.spinosi94@gmail.com>
2020-08-25 17:13:12 +0200

    seconda parte

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var surname = prompt("Inserisci cognome");
 
-var nameList = [surname, "Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+var nameList = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+nameList.push(surname);
 
-for(var i = 0; i < nameList.length; i++){
-  var content = document.getElementById('list').innerHTML;
-  content = content + "<li>" + nameList[i] + "</li>";
-  nameList.sort();
-  document.getElementById('list').innerHTML = content;
+nameList.sort();
 
-}
+var content = document.getElementById('list').innerHTML;
+content = content + nameList;
+document.getElementById('list').innerHTML = content;
+
 
 // document.write(nameList.sort());
 // document.getElementById('list').innerHTML = content;
